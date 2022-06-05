a4333d376c5dc382107bd34bee069460ae5c0d52
Eliam LOTONGA <e.m.lotonga@gmail.com>
2022-06-05 21:17:33 +0200

    Review: fix code, add autocomplete choix, fix css

--- src/js/InputElement.js
+++ src/js/InputElement.js
@@ -1,79 +1,57 @@
+
+
+const Fields = {
+  VALUE: "value",
+  BIND: "bind",
+  HEIGHT: "height",
+  WIDTH: "width",
+  PLACEHOLDER: "placeholder",
+  AUTOCOMPLETE: "autocomplete"
+}
 export default class InputElement extends HTMLElement{
 
   #value = null
+  #bind = null
   #height = null
   #width = null
   #placeholder = null
-  #bind = null
+  #autocomplete = false
   instance = null
 
-  constructor({bind=null, value="", height="45px", width="calc(var(--height) * 4.8)", placeholder="Enter some text..."}={}){
+  constructor({bind=null, value="", height="45px", width="calc(var(--height) * 4.8)", placeholder="Enter some text...", autocomplete=false}={}){
     super()
+
     this.shadow = this.attachShadow({mode: "open"})
-    this.bind = this.getAttribute("bind") ?? bind
-    this.value = this.bind ? eval(this.bind) : this.getAttribute("value") ?? value
-    this.height = this.getAttribute("height") ?? height
-    this.width = this.getAttribute("width") ?? width
+
+    this.bind = this.getAttribute("bind") || bind
+    this.value = this.bind ? eval(this.bind) : this.getAttribute("value") || value
+    this.height = this.getAttribute("height") || height
+    this.width = this.getAttribute("width") || width
     this.placeholder = this.getAttribute("placeholder") ?? placeholder
+    this.autocomplete = this.getAttribute("autocomplete")  || autocomplete
 
     this.group = document.createElement("div")
     this.group.setAttribute("id", "input-group")
     this.group.setAttribute("class", "input-group")
+
     this.icon = document.createElement("span")
     this.icon.id = "input-icon"
     this.icon.setAttribute("class", "input-icon")
+
     this.input = document.createElement("input")
     this.input.setAttribute("type", "text")
     this.input.setAttribute("id", "input")
     this.input.setAttribute("class", "input-control")
-    this.input.setAttribute("autocomplete", "off")
 
-    this._initInstance()
+    this.#initInstance()
   }
 
   static get observedAttributes() {
-    return ['placeholder', 'height', "width", "value"]; 
+    return [ Fields.PLACEHOLDER, Fields.AUTOCOMPLETE, Fields.HEIGHT, Fields.WIDTH, Fields.VALUE ]
   }
 
-
-  /**
-   * Add proxy on instance variable
-   */
-  _initInstance(){
-    this.instance = new Proxy(this,{
-      set(self, key, value ){
-        if(Reflect.has(self, key)){
-          Reflect.set(self, key, value);
-          self.input.setAttribute(key, value)
-          
-          self.render() 
-        }
-        return true;
-      },
-      get(self, key){
-        return Reflect.get(self, key)
-      }
-    });
-  }
-
-
-  #_addStyles(){
-    const style = document.createElement("style")
-    style.innerText = `
-    *{
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-    .content{
-      display: flex;
-      justify-content: center;
-      align-items: flex-start;
-      padding-top: 2em;
-      min-height: 100vh;
-      background: linear-gradient(to top, rgb(25, 14, 63), rgb(72, 47, 155));
-    }
-    .input-group{
+  get #groupStyle(){
+    return `.input-group{
       --height: ${this.height};
       --extended-width: ${this.width};
       --padding: .4em;
@@ -85,16 +63,17 @@ export default class InputElement extends HTMLElement{
       display: flex;
       align-items: center;
       justify-content: center;
-      flex: none;
       height: var(--height);
       min-width: var(--height);
-      padding: var(--padding) 0;
       background: linear-gradient(to right, var(--bg-start-color), var(--bg-end-color));
       border: 1px solid var(--border-color);
       border-radius: calc((var(--height) / 2));
       outline: none;
-    }
-    .input-control{
+    }`
+  }
+
+  get #inputStyle(){
+    return `.input-control{
       height: 100%;
       width: 0;
       padding: 0;
@@ -116,14 +95,18 @@ export default class InputElement extends HTMLElement{
     }
     .input-control::placeholder{
       color: #e2e2e2;
-    }
-    .input-icon{
+    }`
+  }
+
+  get #iconStyle(){
+    return `.input-icon{
       --padding: 1.4em;
 
       display: flex;
       align-items: center;
       justify-content: center;
-      width: calc(var(--height) * 0.7 );
+      width: calc(var(--height) * 0.9 );
+      height: calc(var(--height) * 0.9 );
       color: var(--text-color);
       cursor: pointer;
       transition: .3s color ease-in-out, .3s padding ease-in-out;
@@ -142,21 +125,69 @@ export default class InputElement extends HTMLElement{
       padding-right: var(--padding);
       padding-left: calc(var(--padding) * 0.7);
     }`
+  }
+
+  /**
+   * Add proxy on instance variable
+   */
+  #initInstance(){
+    this.instance = new Proxy(this,{
+      set(self, key, value ){
+        if(Reflect.has(self, key)){
+
+          if([Fields.PLACEHOLDER, Fields.VALUE].includes(key))
+            self.input.setAttribute(key, value)
+
+          if(key === Fields.WIDTH && !value)
+            value = "calc(var(--height) * 4.8)"
+
+          Reflect.set(self, key, value);
+          
+          self.render() 
+        }
+        return true
+      },
+      get(self, key){
+        return Reflect.get(self, key)
+      }
+    })
+  }
+
+  #addStyles(){
+    const style = document.createElement("style")
+    style.innerText = `
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    .content{
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      padding-top: 2em;
+      min-height: 100vh;
+      background: linear-gradient(to top, rgb(25, 14, 63), rgb(72, 47, 155));
+    }
+    ${this.#groupStyle}
+    ${this.#inputStyle}
+    ${this.#iconStyle}
+    `
     this.shadow.appendChild(style)
   }
 
-
   connectedCallback(){
-    this.icon.addEventListener('pointerdown', () => {
+    this.icon.addEventListener('pointerdown', (e) => {
+      e.preventDefault()
       this.input.focus()
     })
-    this.input.addEventListener("change", function(){
+
+    this.input.addEventListener("change", function(e){
       this.setAttribute("value", this.value)
     })
-    this.render();
+    this.shadow.innerHTML = ""
+    this.render()
   }
-
-  
 
   attributeChangedCallback(name, oldValue, newValue) {
     if(oldValue !== newValue){
@@ -164,9 +195,8 @@ export default class InputElement extends HTMLElement{
     }
   }
 
-
   render(){
-    this.#_addStyles()
+    this.#addStyles()
     this.icon.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
       <path fill="currentColor" d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"/>
@@ -174,6 +204,9 @@ export default class InputElement extends HTMLElement{
     `
     this.input.setAttribute("placeholder", this.placeholder)
     this.input.setAttribute("value", this.value)
+    if(!this.autocomplete)
+      this.input.setAttribute("autocomplete",  "off")
+
     this.group.appendChild(this.input)
     this.group.appendChild(this.icon)
     this.shadow.appendChild(this.group)
